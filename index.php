01ab4e66ed7b365f47b90fe41c67ea4b302d38cc
sustudy <1179803351@qq.com>
2015-01-31 12:11:31 +0800

    superssh version1.0

---
+++ index.php
@@ -0,0 +1,212 @@
+<!doctype html>
+    <!--<one line to give the program's name and a brief idea of what it does.>
+    该程序可以是浏览器版的ssh。可以用手机，电脑，平板使用浏览器登陆ssh,来操作linux系统。
+    Copyright (C) 2015 - 2016  苏少峰
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.-->
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+		<script type="text/javascript" src="./jquery-2.0.3.min.js"></script>
+		<title>通用ssh</title>
+		<style type="text/css">
+			*{
+				padding: 0px;
+				margin: 0px;
+			}
+		</style>
+		<script type="text/javascript">
+			var url = "ws://192.168.20.25:9999";
+			var ws = new WebSocket(url);
+			ws.onopen = function(){
+				console.log("success success !!");
+				ws.send("i will send message to localhost");
+			}
+			ws.onmessage = function(e){
+				var message = e.data;
+				var len = 0;
+				if(message == 'yes'){
+					alert('连接成功');
+					var remoteIp = $('#remoteIp').val();
+					var remoteName = $('#remoteName').val();
+					var changeRemoteIp = remoteIp.replace(/\./g, "-");
+					var sshData = "------------------------------->\r\n------------------------------->\r\n------------------------------->\r\n------------------------------->\r\n------------------------------->";
+					var ele = "<input id='"+changeRemoteIp+'_'+remoteName+"_data' type='hidden' value='"+sshData+"'/>";
+					$("body").append(ele);
+					var act = setAct() ? 'act' : '' ;
+					var newSsh = '<li><a style="cursor:pointer" onclick="changeSsh(this)" class="'+act+'" id="'+changeRemoteIp+'_'+remoteName+'">'+remoteIp+'_'+remoteName+'</a></li>';
+					$("#sshList").append(newSsh);
+					closeLogin();
+				}else if(message == 'no'){
+					alert('链接失败');
+				}else{
+					if(message != "complete"){
+						var content = $("#sshCommand").val();
+						var addStr = "\n" + message;
+						var newContent = content + addStr;
+						$("#sshCommand").val(newContent);
+					}else{
+						var content = $("#sshCommand").val();
+						addStr = "------------------------------->";
+						var newContent = content + addStr;
+						$("#sshCommand").val(newContent);
+					}
+				}
+			}
+			ws.onclose = function(){
+				alert("服务器已经断开连接");
+			}
+			ws.onerror = function(e){
+				alert('服务器已断开'+e.data);
+			}
+		</script>
+	</head>
+	<body>
+			<div id="mainView" style="width:100%;height:auto">
+				<div style="background-color:blue;height:100%;width:80%;float:left">
+					<div id="sshIp" class="col-md-12 text-center" style="background-color:white;color:black;height:40px;line-height:40px;border-bottom:3px solid white;text-align:center">192.168.30.3--root</div>	
+					<div id="sshContent" class="col-md-12" style="background-color:white;color:black;">
+						<textarea id="sshCommand" style="background-color:white;color:black;height:100%;width:100%">
+------------------------------->
+------------------------------->
+------------------------------->
+------------------------------->
+------------------------------->
+------------------------------->
+-------------------------------></textarea>
+					</div>	
+				</div>
+				<div style="background-color:white;color:black;height:100%;width:20%;float:left">
+					<a onclick="openLogin()" style="font-size:20px;color:blue;cursor:pointer">新建链接</a>
+					<ul id="sshList" style="list-style:none;margin-top:50px;">
+						<!--<li><a onclick="changeSsh(this)" id="192.168.20.21_root">192.168.20.21_root</a></li>
+						<li><a onclick="changeSsh(this)" id="192.168.30.21_mary">192.168.30.21_mary</a></li>
+						<li><a onclick="changeSsh(this)" id="192.168.20.22_jack">192.168.20.22_jack</a></li>
+						<li><a onclick="changeSsh(this)" id="192.168.20.25_root">192.168.20.25_root</a></li>
+					--></ul>
+				</div>
+				<div style="clear:both"></div>
+			</div>
+	</body>
+	<script type="text/javascript">
+		function openLogin(){
+			var ele = "<div id='allLogin' style='left:0px;top:0px;width:100%;height:100%;position:fixed;background-color:black'>";
+			ele += "<div style='width:300px;height:200px;position:fixed;top:40%;left:40%;background-color:white'>";
+			ele += "远程ip：<input id='remoteIp' type='text' value='192.168.20.25' /><br/>";
+			ele += "用户名：<input id='remoteName' type='text' value='root' /><br/>";
+			ele += "密码：<input id='remotePwd' type='text' value='123456' /><br/>";
+			ele += "<button onclick='checkLogin()'>链接</button>&nbsp;&nbsp;&nbsp;&nbsp;";
+			ele += "<button onclick='closeLogin()'>关闭</button>";
+			ele += "</div>";
+			ele += "</div>";
+			$("body").append(ele);	
+		}
+		function closeLogin(){
+			$("#allLogin").remove();
+		}
+		function checkLogin(){
+			if(true){
+				addSsh();
+			}
+		}
+		function setAct(){
+			var num = $("#sshList").children();
+			if(num.length == 0){
+				return true;
+			}else{
+				return false;
+			};
+		}
+		function addSsh(){
+			var remoteIp = $('#remoteIp').val();
+			var remoteName = $('#remoteName').val();
+			var remotePwd = $('#remotePwd').val();
+			var changeRemoteIp = remoteIp.replace(/\./g, "-");
+			var msg = remoteIp + "#" + remoteName + "#" + remotePwd;
+			ws.send(msg);
+		}
+		function changeSsh(obj){
+			var content = $("#sshCommand").val();
+			var oldSshDataId = $("#sshList a[class*='act']").attr("id");
+			oldSshDataId = oldSshDataId + "_data";
+			$("#"+oldSshDataId).val(content);
+
+			$("#sshList a").removeClass('act');
+			var sshDataId = $(obj).attr("id");
+			sshDataId = sshDataId+"_data";
+			$("#sshCommand").val($("#"+sshDataId).val());
+			$(obj).addClass('act');
+			var ipStr = $(obj).text();
+			$("#sshIp").text(ipStr);
+		}
+	</script>
+	<script type="text/javascript">
+		var width = $(document).width();
+		var height = $(document).height();
+		$("#mainView").css({"width":width, "height":height});
+		$("#sshContent").scrollTop(300);
+		var sshIpHeight = $("#sshIp").height();
+		var sshContentHeight = height - sshIpHeight;
+		$("#sshContent").css({"height":sshContentHeight});
+		$("#sshCommand").keydown(function(event){
+			var kcode = event.keyCode;
+			var sshNum = $("#sshList li").length;
+			if(sshNum == 0){
+				return false;
+			}
+			if(kcode == 37 || kcode == 38 || kcode == 39 || kcode == 40){
+				return true;
+			}
+			if(kcode == 13){
+				var content = $("#sshCommand").val();
+				var oldSshDataId = $("#sshList a[class*='act']").attr("id");
+				oldSshDataId = oldSshDataId + "_data";
+				$("#"+oldSshDataId).val(content);
+				var currentSshId = $("#sshList a[class*='act']").attr('id');
+				currentSshId2 = currentSshId.replace(/-/g, ".");
+				remoteSshInfo = currentSshId2.split('_');
+				var remoteIp = remoteSshInfo[0];
+				var remoteName = remoteSshInfo[1];
+				//获取linux命令
+				var sshDataId = "#" + currentSshId + "_data";
+				var content = $(sshDataId).val();
+				var command = content.split("\n");
+				command = command[command.length-1];
+				command = command.replace(/------------------------------->/, '');
+				var message = remoteIp+'#'+remoteName+'#'+command;
+				ws.send(message);
+				return false;
+			}
+			var startFlag = this.selectionStart;
+			if(event.keyCode == 8){
+				var content = this.value;
+				if(content[startFlag-1] == ">" && content[startFlag-2] == "-"){
+					return false;
+				}
+			}
+			var content = this.value;
+			var str = content.split('\n');
+			var endStr = str[str.length-1];
+			var lowStr = content.length - endStr.length + 10;
+			if(lowStr > startFlag){
+				return false;
+			}else{
+				return true;
+			}
+		});
+	</script>
+</html>
+
+
+
---
+++ server.php
@@ -0,0 +1,185 @@
+<?php
+    /*<one line to give the program's name and a brief idea of what it does.>
+    该程序可以是浏览器版的ssh。可以用手机，电脑，平板使用浏览器登陆ssh,来操作linux系统。
+    Copyright (C) 2015 - 2016  苏少峰
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+	header('content-type=text/html;charset=utf8');
+	class WS{
+		private $clients = array();
+		private $sockets = array();
+		private $clientsInfos = array();
+		private $master;
+		private $debug = true;
+
+		function __construct($ip){
+			$this->master=socket_create(AF_INET, SOCK_STREAM, SOL_TCP)     or die("socket_create() failed");
+			socket_set_option($this->master, SOL_SOCKET, SO_REUSEADDR, 1)  or die("socket_option() failed");
+			socket_bind($this->master, $ip, 9999)                    or die("socket_bind() failed");
+			socket_listen($this->master,20)                                or die("socket_listen() failed");
+			$this->sockets[] = $this->master;
+			while(true){
+				$reads = $this->sockets;
+				socket_select($reads, $a=null, $b=null, 0);
+				if(in_array($this->master, $reads)){
+					$client = socket_accept($this->master);
+					$this->sockets[] = $client;
+	                $bytes = @socket_recv($client, $buffer, 2048, 0);
+	                $this->doHandShake($client, $buffer);
+				}
+				foreach ($reads as $client) {
+					if($this->master == $client){
+						continue;
+					}
+					$recvByte = socket_recv($client, $buffer, 2048, 0);
+					socket_getpeername($client, $ip);
+					if($recvByte == 8){
+						$this->closeConnect($client, $ip);
+						continue;
+					}
+					$message = $this->decode($buffer);
+					$infos = explode("#", $message);
+					$remoteIp = $infos[0];
+					$remoteName = $infos[1];
+					$remotePwdOrCommand = $infos[2];
+					if($this->clientsInfos[$ip][$remoteIp][$remoteName]['isLogin'] == true){
+						$cmd = $remotePwdOrCommand;
+						$connection = $this->clientsInfos[$ip][$remoteIp][$remoteName]['sshConn'];
+						$shell = $this->clientsInfos[$ip][$remoteIp][$remoteName]['shell'];
+						fwrite($shell, $cmd."\n");
+						sleep(0.5);
+						$contentArr = array();
+					    while($line = fgets($shell)) {
+					       	$contentArr[] = $line;
+					    }
+					    $this->sendAllMessage($client, $contentArr);
+					    $this->send($client, "complete");
+					}else if(($this->clientsInfos[$ip][$remoteIp][$remoteName]['isLogin'] == false || empty($this->clientsInfos[$ip][$remoteIp][$remoteName]['isLogin'])) && ($remoteName != "")){
+						$connection = ssh2_connect($remoteIp, 22);
+						$flag = ssh2_auth_password($connection, $remoteName, $remotePwdOrCommand);
+						if($flag){
+							$shell = ssh2_shell($connection, 'xterm');
+							$this->clientsInfos[$ip][$remoteIp][$remoteName]['sshConn'] = $connection;
+							$this->clientsInfos[$ip][$remoteIp][$remoteName]['isLogin'] = true;
+							$this->clientsInfos[$ip][$remoteIp][$remoteName]['shell'] = $shell;
+							$this->send($client, "yes");
+						}else{
+							$this->clientsInfos[$ip][$remoteIp][$remoteName]['isLogin'] == false;
+							$this->send($client, "no");
+						}
+					}
+				}
+			}		
+		}
+		function sendAllMessage($client, $contentArr){
+			foreach ($contentArr as $key => $content) {
+			   	$content = str_replace("[00;34", "", $content);
+			    $content = str_replace("[00m", "", $content);
+			    $content = str_replace("[root@localhost ~]#", "", $content);
+			    $content = str_replace(";root@localhost:~", "", $content);
+			    $content = str_replace("[m]0", "", $content);
+			    $content = str_replace("]0", "", $content);
+			    $content = str_replace("[00;31", "", $content);
+			    $content = str_replace("ls", "", $content);
+			    $content = str_replace("[m", "", $content);
+			    $content = str_replace("", "", $content);
+			    $content = str_replace("", "", $content);
+			    $content = str_replace("\r\n", "", $content);
+		       	$this->send($client, $content);
+			}
+		}
+		function send($client, $msg){
+			$this->log("> " . $msg);
+			$msg = $this->frame($msg);
+			socket_write($client, $msg, strlen($msg));
+			$this->log("! " . strlen($msg));
+			$errorcode = socket_last_error();
+			$errormsg = socket_strerror($errorcode);
+			echo "[$errorcode]".$errormsg."\n";
+		}
+		function closeConnect($socket, $ip){
+			$index = array_search($socket, $this->sockets);
+			socket_close($socket);
+			if ($index >= 0){
+	            array_splice($this->sockets, $index, 1);
+			}
+			unset($this->clientsInfos[$ip]);
+		}
+		function doHandShake($socket, $buffer){
+			$this->log("\nRequesting handshake...");
+			$this->log($buffer);
+			list($resource, $host, $origin, $key) = $this->getHeaders($buffer);
+			$this->log("Handshaking...");
+			$upgrade  = "HTTP/1.1 101 Switching Protocol\r\n" .
+						"Upgrade: websocket\r\n" .
+						"Connection: Upgrade\r\n" .
+						"Sec-WebSocket-Accept: " . $this->calcKey($key) . "\r\n\r\n";  //必须以两个回车结尾
+			$this->log($upgrade);
+	        $sent = socket_write($socket, $upgrade, strlen($upgrade));
+			$this->log("Done handshaking...");
+			return true;
+		}
+		function getHeaders($req){
+			$r = $h = $o = $key = null;
+			if (preg_match("/GET (.*) HTTP/"              ,$req,$match)) { $r = $match[1]; }
+			if (preg_match("/Host: (.*)\r\n/"             ,$req,$match)) { $h = $match[1]; }
+			if (preg_match("/Origin: (.*)\r\n/"           ,$req,$match)) { $o = $match[1]; }
+			if (preg_match("/Sec-WebSocket-Key: (.*)\r\n/",$req,$match)) { $key = $match[1]; }
+			return array($r, $h, $o, $key);
+		}
+		function calcKey($key){
+			$accept = base64_encode(sha1($key . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11', true));
+			return $accept;
+		}
+		function decode($buffer) {
+			$len = $masks = $data = $decoded = null;
+			$len = ord($buffer[1]) & 127;
+
+			if ($len === 126) {
+				$masks = substr($buffer, 4, 4);
+				$data = substr($buffer, 8);
+			} 
+			else if ($len === 127) {
+				$masks = substr($buffer, 10, 4);
+				$data = substr($buffer, 14);
+			} 
+			else {
+				$masks = substr($buffer, 2, 4);
+				$data = substr($buffer, 6);
+			}
+			for ($index = 0; $index < strlen($data); $index++) {
+				$decoded .= $data[$index] ^ $masks[$index % 4];
+			}
+			return $decoded;
+		}
+		function frame($s){
+			$a = str_split($s, 125);
+			if (count($a) == 1){
+				return "\x81" . chr(strlen($a[0])) . $a[0];
+			}
+			$ns = "";
+			foreach ($a as $o){
+				$ns .= "\x81" . chr(strlen($o)) . $o;
+			}
+			return $ns;
+		}
+		function log($msg = ""){
+			if ($this->debug){
+				echo $msg . "\n";
+			} 
+		}
+	}
+
+	$ip = "192.168.20.25";
+	$ws = new WS($ip);
